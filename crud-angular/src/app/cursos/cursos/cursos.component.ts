9827032205abbc8af9625e2ec75819d72d477858
Flavia Jaconis <fla_njaconis@yahoo.com.br>
2023-06-11 17:28:17 -0300

    Construindo a tabela de cursos

--- crud-angular/src/app/cursos/cursos/cursos.component.ts
+++ crud-angular/src/app/cursos/cursos/cursos.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Curso } from '../model/curso';
 
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
   styleUrls: ['./cursos.component.scss']
 })
-export class CursosComponent {
+export class CursosComponent implements OnInit {
+
+  cursos: Curso[] = []; //poderia ser inicializada assim
+  constructor(){
+    //this.cursos = []; ou poderia ser inicializada assim
+  }
+
+  ngOnInit(): void {
+
+  }
 
 }
